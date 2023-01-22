683b7daf1ed7f47dcf55225a6b7a76cf2f07b22d
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-01-22 11:56:47 +0700

    add laporan table

---
+++ models/laporanModel.js
@@ -0,0 +1,26 @@
+import {Sequelize} from "sequelize";
+import db from "../config/Database.js";
+
+const {DataTypes} = Sequelize;
+
+const Laporan = db.define('tbl_laporan',{
+    nama: DataTypes.STRING,
+    email: DataTypes.STRING,
+    jenis_laporan: DataTypes.STRING,
+    judul_laporan: DataTypes.STRING,
+    isi_laporan: DataTypes.TEXT,
+    createdAt: {
+        type: DataTypes.DATEONLY,
+    },
+    updatedAt: {
+        type: DataTypes.DATEONLY,
+    }
+},{
+    freezeTableName: true
+});
+
+export default Laporan;
+
+(async()=>{
+    await db.sync();
+})();
