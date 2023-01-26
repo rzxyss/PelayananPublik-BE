b6c3a21f6ab0c15441b795f56d065f860a69f809
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-01-26 15:15:59 +0700

    add totalRows

--- controllers/laporanController.js
+++ controllers/laporanController.js
@@ -9,6 +9,70 @@ export const getLaporan = async (req, res) => {
   }
 };
 
+export const getPengaduan = async (req, res) => {
+  try {
+    const response = await Laporan.findAll({
+      where: {
+        jenis_laporan: "Pengaduan",
+      },
+    });
+    const count = await Laporan.count({
+      where: {
+        jenis_laporan: "Pengaduan",
+      },
+    });
+    res.json({
+      result: response,
+      totalRows: count,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getAspirasi = async (req, res) => {
+  try {
+    const response = await Laporan.findAll({
+      where: {
+        jenis_laporan: "Aspirasi",
+      },
+    });
+    const count = await Laporan.count({
+      where: {
+        jenis_laporan: "Aspirasi",
+      },
+    });
+    res.json({
+      result: response,
+      totalRows: count,
+    });
+    res.json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getInformasi = async (req, res) => {
+  try {
+    const response = await Laporan.findAll({
+      where: {
+        jenis_laporan: "Permintaan Informasi",
+      },
+    });
+    const count = await Laporan.count({
+      where: {
+        jenis_laporan: "Permintaan Informasi",
+      },
+    });
+    res.json({
+      result: response,
+      totalRows: count,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 export const detailLaporan = async (req, res) => {
   try {
     const response = await Laporan.findOne({
