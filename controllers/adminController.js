fa84f976fe34ccf3f5e62ff4a2707c1785214f9b
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-01-26 15:14:43 +0700

    memperbaiki system login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,45 +1,83 @@
 import Admin from "../models/adminModel.js";
-
-export const adminToken = async (req, res) => {
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+export const checkToken = async (req, res) => {
   try {
     const response = await Admin.findOne({
       where: {
         token: req.body.token,
-      },
+      }, attributes: ['name', 'token']
     });
-    res.status(200).json(response.token);
+    // res.json(response)
+    if (!response) {
+      res.status(400).send("Invalid Credentials");
+    } else {
+      res.json(response)
+    }
   } catch (error) {
-    res.status(402).json(error)
+    res.status(402).json(error);
   }
 };
 
 export const loginAdmin = async (req, res) => {
   try {
-    const response = await Admin.findOne({
+    const { username, password } = req.body;
+    if (!(username && password)) {
+      res.status(400).send("All input is required");
+    }
+    const admin = await Admin.findOne({
       where: {
-        username: req.body.username,
-        password: req.body.password,
+        username,
       },
     });
-    if (!response) {
-      res.status(402).json("success false");
-    } else {
-      res.status(200).json(response.id);
-        const idAdmin = response.id
-        await Admin.update(
-          {
-            token: req.body.token,
+
+    if (admin && (await bcrypt.compare(password, admin.password))) {
+      // Create token
+      const token = jwt.sign(
+        { id: admin.id, username },
+        process.env.ACCESS_TOKEN_SECRET
+      );
+      admin.token = token;
+      res.cookie("accessToken", token, {
+        httpOnly: false,
+      });
+      await Admin.update(
+        { token: token },
+        {
+          where: {
+            id: admin.id,
           },
-          {
-            where: {
-              id: idAdmin,
-            },
-          }
-        );
+        }
+      );
+      res.json({ token });
     }
-  } catch (error) {
-    console.log(error.message);
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
   }
+  // try {
+  //   const { username, password } = req.body;
+  //   const response = await Admin.findOne({
+  //     where: {
+  //       username,
+  //     },
+  //   });
+  //   if (!response) {
+  //     res.status(402).json("success false");
+  //   } else {
+  //     if (response && (await bcrypt.compare(password, response.password))) {
+  //       const token = jwt.sign(
+  //         { id: response.id, username },
+  //         process.env.ACCESS_TOKEN_SECRET
+  //       );
+  //       response.token = token;
+  //       res.json({token})
+  //     }
+  //   }
+  //   // res.status(200).json({token});
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
 };
 
 export const logoutAdmin = async (req, res) => {
@@ -49,7 +87,7 @@ export const logoutAdmin = async (req, res) => {
         token: req.body.token,
       },
     });
-    res.json(response.token)
+    res.json(response.token);
     await Admin.update(
       { token: null },
       {
@@ -67,8 +105,14 @@ export const addAdmin = async (req, res) => {
   const name = req.body.name;
   const username = req.body.username;
   const password = req.body.password;
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
   try {
-    await Admin.create({ name: name, username: username, password: password });
+    await Admin.create({
+      name: name,
+      username: username,
+      password: hashPassword,
+    });
     res.status(201).json({ msg: "Admin Berhasil Dibuat" });
   } catch (error) {
     console.log(error.message);
