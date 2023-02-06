0ead54eba37bd1d88a801bde24de53cd5fc3d2e2
Rizki Saepul Aziz <rzkysa0@gmail.com>
2023-02-06 13:36:54 +0700

    change database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('pelayanan_publik', 'root', 'tikomdikceria',{
-    host: 'localhost',
+const db = new Sequelize('railway', 'root', '7PLKSkE9n8mKbxBTKlJM',{
+    host: 'containers-us-west-164.railway.app',
+    port: 6320,
     dialect: "mysql",
     timezone: '+07:00'
 });
